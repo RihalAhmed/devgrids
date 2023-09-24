9d9fe8d0e6e6a05753220cf48a1282fd8663e5fe
Mohammad Fathi Rahman <md.fathi.1999@gmail.com>
2023-09-24 20:21:00 +0600

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-wrfmqgsl6jouep&o5*yo)cw9m!3^dq#az*8ivjif4)zw8@!n1=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
